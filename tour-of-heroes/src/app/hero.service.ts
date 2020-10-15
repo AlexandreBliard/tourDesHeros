e50f5aaada149536dcaa0a8d266ca84c08864971
Alexandre.Bliard <alexandre.bliard@le-campus-numerique.fr>
2020-10-15 16:58:41 +0200

    add Services ok

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import {Observable, of} from "rxjs";
+import {MessageService} from "./message.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    //envois le message après être allé chercher les héros
+    this.messageService.add('HeroService: fetched heroes');
+  return of (HEROES);
+}
+
 }
